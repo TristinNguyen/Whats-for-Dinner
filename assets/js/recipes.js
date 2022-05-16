8e4caa929d865218b91f569383cf0a5240efcf42
Tristin Nguyen <nguye501@msu.edu>
2022-05-16 19:01:26 -0400

    merge request

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -12,11 +12,7 @@ function getRecipe(query) {
     })
     .then(function (data) {
       // console.log(data.meals)
-<<<<<<< HEAD
-      for (var i = 0; i < data.meals.length; i++) {
-=======
       for (var i = 0; i< data.meals.length; i++) {
->>>>>>> 89b9f42f4329ed8d966131a3a92cd676150ac035
         console.log(data.meals[i])
         // data.meals[i].idMeal
 
@@ -25,11 +21,7 @@ function getRecipe(query) {
         image.setAttribute("src", data.meals[i].strMealThumb)
         // document.querySelector("#display-recipe").append(image)
         var mealCard = document.createElement("div")
-<<<<<<< HEAD
-          mealCard.innerHTML =
-=======
         mealCard.innerHTML =
->>>>>>> 89b9f42f4329ed8d966131a3a92cd676150ac035
           `<div class="card" style="width: 500px;">
                 <div class="card-divider">
                   ${data.meals[i].strMeal}
@@ -39,15 +31,8 @@ function getRecipe(query) {
                   <a href= fullrecipe.html>Click here to review recipe.</a>
                 </div>
               </div>`
-<<<<<<< HEAD
-          document.querySelector("#display-recipe").append(mealCard)
-
-      }
-
-=======
         document.querySelector("#display-recipe").append(mealCard)
       }
->>>>>>> 89b9f42f4329ed8d966131a3a92cd676150ac035
     })
 }
 document.querySelector("#submit-ingr").addEventListener("click", formSubmitHandler)
