520ecc8a9d60e22c0309e465643bd53a64303405
Bre <breanna.schmidt12@gmail.com>
2022-05-17 22:22:42 -0400

    attempt at save function

--- assets/js/cocktails.js
+++ assets/js/cocktails.js
@@ -1,3 +1,4 @@
+var save = document.getElementById("#save");
 
 function getDrink() {
   var requestDrink = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
@@ -18,3 +19,16 @@ function getDrink() {
 }
 
 getDrink();
+
+function saveDrink() {
+    var saveName = document.getElementById("#movetofavs");
+    var savePic = document.getElementById("#movetofavs");
+    saveName.setAttribute("src", data.drinks[0].strDrink);
+    savePic.setAttribute("src", data.drinks[0].strDrinkThumb);
+    localStorage.setItem('saveName, savePic',JSON.stringify(saveName,savePic));
+    var retrieveItem = JSON.parse(localStorage.getItem(saveName,savePic));
+    console.log('retrieveItem: ', retrieveItem)
+}
+saveDrink();
+
+document.querySelector("#save").addEventListener("click",saveDrink)
